7cb649438c39f855449ee5e73f9457277c746bff
Prem Sagar S <reallypremsagar@gmail.com>
2022-11-08 12:19:19 +0530

    Transfer funds to mock government contract

--- scripts/deploy.py
+++ scripts/deploy.py
@@ -5,7 +5,8 @@ from .helpful import get_account, update_front_end
 def main():
     account = get_account()
     if len(GovtMock) == 0:
-        GovtMock.deploy(
+        govt = GovtMock.deploy(
             config["networks"][network.show_active()]["gasreserve"], {"from": account}
         )
+        govt.transfer(100000000000000000)
     update_front_end()
